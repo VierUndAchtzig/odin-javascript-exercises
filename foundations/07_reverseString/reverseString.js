9ad2e846f3538ad47184942323c41f651448efa8
VierUndAchtzig <202284300+VierUndAchtzig@users.noreply.github.com>
2026-04-03 03:17:54 +0200

    Finish foundations exercises 1 to 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let wort = ""
+  for (i = string.length - 1; i >= 0; i--) {
+    // string.at(i) += wort
+    wort += string.at(i)
+  }
+  return wort
 };
 
 // Do not edit below this line
